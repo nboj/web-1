151820764a35b9c9bd0700f35d2bc5bad3cddacd
nboj <60493251+nboj@users.noreply.github.com>
2025-12-08 00:13:38 -0500

    u

--- project-3/sign-out/script.js
+++ project-3/sign-out/script.js
@@ -2,5 +2,6 @@ document.addEventListener('DOMContentLoaded', (_e) => {
     sessionStorage.setItem("signed-in", false);
     sessionStorage.setItem("got-discount", false);
     sessionStorage.setItem("discount", null);
-    window.location.href = "/project-3/sign-in";
+    const href = new URL("/project-3/sign-in", import.meta.url);
+    window.location.href = href;
 })
